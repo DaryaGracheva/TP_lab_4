2ba1aabf540ab4bd0cc7b543ea65d8eaa33121c1
DaryaGracheva <99476603+DaryaGracheva@users.noreply.github.com>
2023-05-16 21:59:21 +0400

    Добавьте файлы проекта.

---
+++ TP_lab_4/laba_1/Models/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace laba_1.Models
+{
+    public class Calc
+    {
+        [Required(ErrorMessage = "Введите число")]
+        public string? operand1 { get; set; }
+        [Required(ErrorMessage = "Введите число")]
+        [Range(-128, 127, ErrorMessage = "Второе число должно быть от -128 до 127.")]
+        public string? operand2 { get; set; }
+        public string result { get; set; }
+        public string calulate { get; set; }
+        public string exception { get; set; }
+    }
+}
